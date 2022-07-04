2402ac8c450c79f0ae6051a11828054205b159b9
MarcoHilliges <107026106+MarcoHilliges@users.noreply.github.com>
2022-07-04 09:41:35 +0200

    responsive ready

--- models/keyboard.class.js
+++ models/keyboard.class.js
@@ -7,19 +7,50 @@ class KeyBoard{
     D = false;
 
     constructor(){
-        this.screenButtonPressEvent();
+        setTimeout(() => {
+            this.screenButtonPressEvent();
+        }, 500);
+        
     }
 
     screenButtonPressEvent(){
-        // document.getElementById('gameButtonJump').addEventListener('touchstart', (e) => {
-        //    e.preventDefault();
-        //    this.SPACE = true; 
-        // });
-        // document.getElementById('gameButtonJump').addEventListener('touchend', (e) => {
-        //     e.preventDefault();
-        //     this.SPACE = false; 
-        //  });
+        document.getElementById('gameButtonJump').addEventListener('touchstart', (e) => {
+           e.preventDefault();
+           this.SPACE = true; 
+        });
+        document.getElementById('gameButtonJump').addEventListener('touchend', (e) => {
+            e.preventDefault();
+            this.SPACE = false; 
+        });
         
+        document.getElementById('gameButtonThrow').addEventListener('touchstart', (e) => {
+            e.preventDefault();
+            this.D = true; 
+        });
+        document.getElementById('gameButtonThrow').addEventListener('touchend', (e) => {
+             e.preventDefault();
+             this.D = false; 
+        });
+
+        document.getElementById('gameButtonLeft').addEventListener('touchstart', (e) => {
+            e.preventDefault();
+            this.LEFT = true; 
+        });
+        document.getElementById('gameButtonLeft').addEventListener('touchend', (e) => {
+             e.preventDefault();
+             this.LEFT = false; 
+        });
+        
+        document.getElementById('gameButtonRight').addEventListener('touchstart', (e) => {
+            e.preventDefault();
+            this.RIGHT = true; 
+         });
+        document.getElementById('gameButtonRight').addEventListener('touchend', (e) => {
+             e.preventDefault();
+             this.RIGHT = false; 
+          });
+
+
 
     }
 }
